461943f81befa982b361d6d777149c125b3ee840
Hyeon_uk <80202054+rkadl9999@users.noreply.github.com>
2023-01-04 19:26:26 +0900

    👍| Constructor 추가

--- hello-spring/src/main/java/Hyeonuk/hellospring/service/MemberService.java
+++ hello-spring/src/main/java/Hyeonuk/hellospring/service/MemberService.java
@@ -2,13 +2,17 @@ package Hyeonuk.hellospring.service;
 
 import Hyeonuk.hellospring.domain.Member;
 import Hyeonuk.hellospring.repository.MemberRepository;
-import Hyeonuk.hellospring.repository.MemoryMemberRepository;
 
 import java.util.List;
 import java.util.Optional;
 
 public class MemberService {
-    private final MemberRepository memberRepository = new MemoryMemberRepository();
+    private final MemberRepository memberRepository;
+
+    public MemberService(MemberRepository memberRepository) {
+        this.memberRepository = memberRepository;
+    }
+
 
     public Long join(Member member) {
         validateDuplicateMember(member); //중복 확인
