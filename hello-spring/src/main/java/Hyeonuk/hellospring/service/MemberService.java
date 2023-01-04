559f4278516d104fa72c3a6978fcdf3976eb620e
Hyeon_uk <80202054+rkadl9999@users.noreply.github.com>
2023-01-04 17:17:23 +0900

    ✨| Add MemberService Logic

---
+++ hello-spring/src/main/java/Hyeonuk/hellospring/service/MemberService.java
@@ -0,0 +1,34 @@
+package Hyeonuk.hellospring.service;
+
+import Hyeonuk.hellospring.domain.Member;
+import Hyeonuk.hellospring.repository.MemberRepository;
+import Hyeonuk.hellospring.repository.MemoryMemberRepository;
+
+import java.util.List;
+import java.util.Optional;
+
+public class MemberService {
+    private final MemberRepository memberRepository = new MemoryMemberRepository();
+
+    public Long join(Member member) {
+        validateDuplicateMember(member); //중복 확인
+        memberRepository.save(member);
+        return member.getId();
+    }
+
+    private void validateDuplicateMember(Member member) {
+        memberRepository.findByName(member.getName())
+            .ifPresent(m -> {
+                throw new IllegalStateException("이미 존재하는 회원입니다.");
+            });
+    }
+
+    //전체 회원 조회
+    public List<Member> findMembers() {
+        return memberRepository.findAll();
+    }
+
+    public Optional<Member> findOne(Long memberId) {
+        return memberRepository.findById(memberId);
+    }
+}
