366fab098102907a63ab4f5e65b45a6ffaf42376
Hyeon_uk <80202054+rkadl9999@users.noreply.github.com>
2023-01-06 15:42:33 +0900

    ⚙️| Component Scan && AutoDI

--- hello-spring/src/main/java/Hyeonuk/hellospring/service/MemberService.java
+++ hello-spring/src/main/java/Hyeonuk/hellospring/service/MemberService.java
@@ -2,13 +2,17 @@ package Hyeonuk.hellospring.service;
 
 import Hyeonuk.hellospring.domain.Member;
 import Hyeonuk.hellospring.repository.MemberRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
 
 import java.util.List;
 import java.util.Optional;
 
+@Service
 public class MemberService {
     private final MemberRepository memberRepository;
 
+    @Autowired
     public MemberService(MemberRepository memberRepository) {
         this.memberRepository = memberRepository;
     }
