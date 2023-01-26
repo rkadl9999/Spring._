d162d13a60700189a109d862d2d1e03513cdcaab
Hyeon_uk <80202054+rkadl9999@users.noreply.github.com>
2023-01-26 23:27:15 +0900

    👍| Join PostMapping

--- Blog/src/main/java/Hyeonuk/Blog/controller/BlogController.java
+++ Blog/src/main/java/Hyeonuk/Blog/controller/BlogController.java
@@ -1,7 +1,10 @@
 package Hyeonuk.Blog.controller;
 
+import Hyeonuk.Blog.domain.MemberForm;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 
 @Controller
 public class BlogController {
@@ -13,5 +16,11 @@ public class BlogController {
     @GetMapping("/join")
     public String join() {
         return "join";
+    }
+
+    @PostMapping("/join")
+    public MemberForm signUp(@RequestBody MemberForm memberForm) {
+        System.out.println(memberForm.getName());
+        return memberForm;
     }
 }
