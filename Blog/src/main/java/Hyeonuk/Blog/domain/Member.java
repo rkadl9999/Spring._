e61bf607754a022146adb34b09770c117583d4d8
Hyeon_uk <80202054+rkadl9999@users.noreply.github.com>
2023-02-02 22:51:56 +0900

    👍| Entity && Id Annotation  추가

--- Blog/src/main/java/Hyeonuk/Blog/domain/Member.java
+++ Blog/src/main/java/Hyeonuk/Blog/domain/Member.java
@@ -1,6 +1,11 @@
 package Hyeonuk.Blog.domain;
 
+import javax.persistence.Entity;
+import javax.persistence.Id;
+
+@Entity
 public class Member {
+    @Id
     private String id;
     private String pwd;
     private String name;
